cf5525a0768df93273ac2d73fc745f42d8938cd0
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-11 19:48:10 +0800

    Update simpleBruteforce.py

--- simpleBruteforce.py
+++ simpleBruteforce.py
@@ -10,7 +10,7 @@ print ("starting..")
 time.sleep(2)
 
 
-def (u= input("Enter Url: ")) 
+def(u= input("Enter Url: ")) 
 expression = "incorrect"
 def brute(username,password):
 	data = {'username':username,'password':password}
